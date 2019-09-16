37d8512090d16b4e631c82d431a511e8958a0924
ZJ <zak@newalexandria.org>
2019-09-15 21:57:08 -0400

    add base class file

---
+++ osx_trash.rb
@@ -0,0 +1,35 @@
+
+class OSXTrash
+
+  attr_reader :path, :results, :errors
+
+  def initialize(opt={})
+    return nil if init_validate(opt)
+
+    @paths = source_paths(opt)
+  end
+
+  def send_to_trash
+    @paths.map do |f|
+      IO.popen("osascript 2>&2", "w") do |stdin|
+        stdin.puts %Q{tell app "Finder" to move the POSIX file "#{f}" to trash}
+      end
+      [$?.to_json, f]
+    end
+  end
+
+  private
+
+  def init_validate(opt)
+    !opt.is_a?(Hash) && (opt[:file] || opt[:paths])
+  end
+
+  def source_paths(opt)
+    paths = opt[:paths] || File.readlines(opt[:file])
+    clean_input(paths)
+  end
+
+  def clean_input(paths)
+    paths.map(&:strip).reject(&:empty?)
+  end
+end
